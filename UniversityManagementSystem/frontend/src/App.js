97ff0d848deb9e32c9086266ccb81ea5da6bc197
AKSHAT SRIVASTAVA <chikusrivastava535@gmail.com>
2025-04-06 17:52:02 +0530

    Update App.js

---
+++ UniversityManagementSystem/frontend/src/App.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files from /public
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Example route
+app.get('/', (req, res) => {
+  res.render('index'); // Make sure you have a file src/views/index.ejs
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
